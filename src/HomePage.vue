86311a0a118f9217010c2f1a16fa20718af91080
Alex Hunter <63908354+al-mister-dog@users.noreply.github.com>
2020-10-06 13:40:47 +0100

    Add files via upload

---
+++ src/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <img :src="img" alt="">
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            img: "../assets/tommys-homepage-img.jpg"
+        }
+    }
+}
+</script>
---
+++ src/TheHeader.vue
@@ -0,0 +1,48 @@
+<template>
+<div>
+  <ul>
+    <li class="item1">Music</li>
+    <li class="item2">Video</li>
+  </ul>
+  <h2>The Thumping Tommys</h2> 
+  <ul>
+    <li class="item1">Shows</li>
+    <li class="item2">Contact</li>
+  </ul>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+div {
+  display: flex;
+  background-color: antiquewhite;
+}
+ul {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  list-style: none;
+  margin: auto;
+  padding: 0;
+}
+.item1 {
+  margin-right: .5rem;
+  padding: .5rem;
+}
+.item2 {
+  margin-left: .5rem;
+  padding: .5rem;
+}
+li:hover {
+  background-color: rgba(0,0,0,0.1);
+  border-radius: 4px;
+}
+</style>
